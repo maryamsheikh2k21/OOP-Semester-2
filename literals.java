9b688eee5e640e734dd1eff034f7a8a2cb1037b7
maryamsheikh2k21 <104642823+maryamsheikh2k21@users.noreply.github.com>
2022-06-02 16:33:16 +0500

    Add files via upload

---
+++ literals.java
@@ -0,0 +1,50 @@
+//program involving all literals types
+public class literals
+{
+public static void main(String args[])
+{
+//byte
+byte a;
+a=89;
+System.out.println(a);
+//short
+short b;
+b=90;
+System.out.println(b);
+
+//int 
+int c;
+c=91;
+System.out.println(c);
+
+//long
+long d;
+d=92;
+System.out.println(d);
+
+//double
+double e;
+e=93;
+System.out.println(e);
+
+//float 
+float f;
+f=94;
+System.out.println(f);
+
+//character 
+char g;
+g='A';
+System.out.println(g);
+
+//string
+String h;
+h="Hello";
+System.out.println(h);
+
+//boolean
+boolean i;
+i=true;
+System.out.println(i);
+}
+}
